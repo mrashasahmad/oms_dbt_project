c5df0d030352eb760848182bbf1fb5c8f093fd22
ashasahmad <ashasahmad@hotmail.com>
2024-11-07 16:54:07 +0500

    added tests and generic tests.

--- models/orders_stg.sql
+++ models/orders_stg.sql
@@ -5,12 +5,7 @@ SELECT
     EmployeeID,
     StoreID,
     Status as StatusCD,
-    CASE
-        WHEN Status = '01' THEN 'In Progress'
-        WHEN Status = '02' THEN 'Completed'
-        WHEN Status = '03' THEN 'Cancelled'
-        ELSE NULL
-    END AS StatusDesc,
+    StatusDesc,
     Updated_at
 FROM
     {{ source('landing', 'ordr') }}
